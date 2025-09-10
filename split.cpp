3e59182a18656d3f643d93439881bcf6fab12166
ajhazime@gmail.com <ahazime@usc.edu>
2025-09-10 21:57:35 +0000

    Initial commit for hw1

--- split.cpp
+++ split.cpp
@@ -2,9 +2,9 @@
 CSCI 104: Homework 1 Problem 1
 
 Write a recursive function to split a sorted singly-linked
-list into two sorted linked lists, where one has the even 
-numbers and the other contains the odd numbers. Students 
-will receive no credit for non-recursive solutions. 
+list into two sorted linked lists, where one has the even
+numbers and the other contains the odd numbers. Students
+will receive no credit for non-recursive solutions.
 To test your program write a separate .cpp file and #include
 split.h.  **Do NOT add main() to this file**.  When you submit
 the function below should be the only one in this file.
@@ -14,22 +14,39 @@ the function below should be the only one in this file.
 
 /* Add a prototype for a helper function here if you need */
 
-void split(Node*& in, Node*& odds, Node*& evens)
+bool isEven(int n);
+
+void split(Node *&in, Node *&odds, Node *&evens)
 {
   /* Add code here */
-// WRITE YOUR CODE HERE
+  // WRITE YOUR CODE HERE
 
-  //Base Case in is empty
-  if(in == NULL){
+  /* Base Case Node_in is empty */
+  if (!in){
     return;
-  }
-  //Even Valules
-  if(in->value == 0 || (in->value % 2) == 0){
-    if(odds == NULL){
-      head = new Node(in->value)
-    }
-    newNode = new Node()
+  } 
+  
+  Node* currentNode = in; //current node 
+  in = in->next; //moving in node by 1 to the right
+  currentNode->next = nullptr; //detaching current node->next to nullptr
+
+  if (isEven(currentNode->value)) { // if the value is even
+    evens = currentNode; //set evens to current node
+    return split(in, odds, evens->next); //pass updated in, and nullptr odds/evens
+  } else {
+    odds = currentNode;
+    return split(in, odds->next, evens);//pass updated in, and nullptr odds/evens
   }
 }
 
 /* If you needed a helper function, write it here */
+
+/* Helper function to determine even/odd numbers */
+bool isEven(int n)
+{
+  if (n % 2 == 0 || n == 0)
+  {
+    return true;
+  }
+  return false;
+}
--- test_split.cpp
+++ test_split.cpp
@@ -8,10 +8,23 @@ Create linked lists and split them with your split() function.
 You can compile this file like this:
 g++ split.cpp test_split.cpp -o test_split
 */
-
+#include <cstddef>
 #include "split.h"
 
 int main(int argc, char* argv[])
 {
+  Node node5(5, NULL);
+  Node node4(4, &node5);
+  Node node3(3, &node4);
+  Node node2(2, &node3);
+  Node node1(1, &node2);
+  Node Node0(0, &node1);
+  Node* head = &Node0;
 
+  Node* evens = nullptr;
+  Node* odds = nullptr;
+
+  split(head, evens, odds);
+  std::cout << heads->value << '\n' << odds->value << std::endl;
+  return 0;
 }
