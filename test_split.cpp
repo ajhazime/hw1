428b0a853d5f05608dab641a8f0a255d97ee4f83
ajhazime <ajhazime@gmail.com>
2025-09-11 15:17:16 -0700

    test_split.cpp is same as codio (NO CHANGES). function prototypes pasted into ulliststr.cpp

--- test_split.cpp
+++ test_split.cpp
@@ -8,16 +8,37 @@ Create linked lists and split them with your split() function.
 You can compile this file like this:
 g++ split.cpp test_split.cpp -o test_split
 */
-
+#include <cstddef>
+#include <iostream>
 #include "split.h"
 
 int main(int argc, char *argv[])
 {
-    Node *linkedListIn = new Node(0, nullptr);
-    for(int i = 0; i < 25; i++)
+
+    // Node node5(5, NULL);
+    // Node node4(4, &node5);
+    // Node node3(3, &node4);
+    // Node node2(2, &node3);
+    // Node node1(1, &node2);
+    // Node node0(0, &node1);
+    // Node* head = &node0;
+    Node *head = nullptr;
+
+    Node *evens = nullptr;
+    Node *odds = nullptr;
+
+    split(head, evens, odds);
+
+    while (evens != nullptr)
     {
-        Node *newNode = new Node(i, nullptr);
+        std::cout << "Evens: " << evens->value << '\n';
+        evens = evens->next;
     }
 
+    while (odds != nullptr)
+    {
+        std::cout << "Odds: " << odds->value << '\n';
+        odds = odds->next;
+    }
     return 0;
 }
